cd647439aa657a19504226f5c2bc1a40f75a6e24
Jakub Molinski <jakub@molinski.dev>
2022-03-17 15:04:10 +0100

    B part

--- zadanie-haskell/HashTree.hs
+++ zadanie-haskell/HashTree.hs
@@ -43,3 +43,41 @@ drawTree t = drawIndentedTree 0 t
     drawTree' i (Twig h l) = " +\n" ++ drawIndentedTree (i + 1) l
     drawTree' i (Node h l r) = " -\n" ++ drawIndentedTree (i + 1) l ++ drawIndentedTree (i + 1) r
 
+type MerklePath = [Either Hash Hash]
+
+data MerkleProof a = MerkleProof a MerklePath
+
+buildProof :: Hashable a => a -> Tree a -> Maybe (MerkleProof a)
+buildProof v t =
+  let paths = merklePaths v t
+   in if null paths
+        then Nothing
+        else Just $ MerkleProof v $ head paths
+
+merklePaths :: Hashable a => a -> Tree a -> [MerklePath]
+merklePaths v (Leaf h _) = [[] | hash v == h]
+merklePaths v (Twig _ (Leaf lh _)) = [[Left lh] | hash v == lh]
+merklePaths v (Twig _ t) = map (Left (treeHash t) :) (merklePaths v t)
+merklePaths v (Node _ (Leaf lh _) (Leaf rh _)) = [[Left rh] | hash v == lh] ++ [[Right lh] | hash v == rh]
+merklePaths v (Node _ l (Leaf rh _)) = (map (Left rh :) (merklePaths v l)) ++ [[Right (treeHash l)] | hash v == rh]
+merklePaths v (Node _ (Leaf lh _) r) = [[Left (treeHash r)] | hash v == lh] ++ (map (Right lh :) (merklePaths v r))
+merklePaths v (Node _ l r) = (map (Left (treeHash r) :) (merklePaths v l)) ++ (map (Right (treeHash l) :) (merklePaths v r))
+
+showMerklePath :: MerklePath -> [Char]
+showMerklePath [] = ""
+showMerklePath (Left h : t) = "<" ++ showHash h ++ showMerklePath t
+showMerklePath (Right h : t) = ">" ++ showHash h ++ showMerklePath t
+
+instance Show a => Show (MerkleProof a) where
+  show (MerkleProof a p) =
+    (showString "(MerkleProof " . showString (show a) . showString " " . showString (showMerklePath p)) ")"
+
+verifyProof :: Hashable a => Hash -> MerkleProof a -> Bool
+verifyProof h (MerkleProof a []) = hash a == h
+verifyProof h (MerkleProof a path) =
+  h == hashPath path
+  where
+    hashPath :: MerklePath -> Hash
+    hashPath [] = hash a
+    hashPath (Left hr : t) = hash (hashPath t, hr)
+    hashPath (Right hl : t) = hash (hl, hashPath t)
