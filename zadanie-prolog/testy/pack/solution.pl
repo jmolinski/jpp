b101f68c4af069395beb305ee55b4d0c4455dec0
Jakub Molinski <jakub@molinski.dev>
2022-06-05 14:22:27 +0200

    aaa

--- zadanie-prolog/testy/pack/solution.pl
+++ zadanie-prolog/testy/pack/solution.pl
@@ -117,12 +117,12 @@ correct(dfa(TF, Q0, F), Representation) :-
     statesFromTF(TF, StatesWithDuplicates),
     sort(StatesWithDuplicates, States),
     % Czy w zbiorze F nie ma powtórzeń?
-    sort(F, AcceptingStates),
-    areListsEqualLength(F, AcceptingStates),
+    %sort(F, AcceptingStates),
+    %areListsEqualLength(F, AcceptingStates),
     % Czy alfabet jest niepusty?
-    head(Alphabet, _),
+    %head(Alphabet, _),
     % Czy stan początkowy jest w zbiorze stanów?
-    member(Q0, States),         
+    %member(Q0, States),         
     % Czy wszystkie stany akceptujące są w zbiorze stanów?
     %allMembers(F, States),
     % Czy funkcja przejścia moze być funkcją? liczba przejść == liczba stanów razy liczba liter TODO
